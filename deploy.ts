62c7a4e272f313220328e533704e7b169af14aa7
Lucas Barros <lbarros.comp@gmail.com>
2021-03-21 01:48:52 -0300

    Inserting multiples stacks

--- deploy.ts
+++ deploy.ts
@@ -1,35 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const {NodeSSH} = require('node-ssh');
-const ssh = new NodeSSH();
-require('dotenv').config();
+const ReactDeploy = require('./stacks/react');
 
 interface ISetupConfig {
-  workingSetup: string, // React, Nodejs, Angularjs
+  workingSetup: string, // React, Nodejs, Nextjs, Angularjs
   workDir?: string, // default: /var/www
   isUsingNginx?: boolean,
   isUsingPM2?: boolean,
   isPrivateKey?: boolean,
+  pm2Setup?: {
+    appName?: string,
+  },
 };
 
 async function run({ workingSetup, workDir, isUsingNginx, isUsingPM2, isPrivateKey }: ISetupConfig) {
-  if (workingSetup === 'React') {
-    console.log('Deploying the React application');
-    // Connecting to the server to put the current build files
-    // ssh.connect({
-    //     host: process.env.SERVER_HOST,
-    //     username: process.env.SERVER_USER,
-    //     privateKey: `${path.join(__dirname, `${process.env.SERVER_KEYFILE_NAME}.pem`)}`,
-    //   })
-    //   .then(() => {
-
-    //     console.log('Connected');
-    //     ssh.execCommand('pm2 list').then((result) => {
-    //       console.log('STDOUT: ' + result.stdout);
-    //       // console.log('STDERR: ' + result.stderr);
-    //     });
-    //   });
-  }
+  switch(workingSetup) {
+    case 'React': {
+      ReactDeploy();
+      break;
+    }
+    default:
+      console.log('Select a valid working setup');
+      break;
+    }
 };
 
 module.exports = {
---
+++ stacks/react.ts
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const {NodeSSH} = require('node-ssh');
+const ssh = new NodeSSH();
+const { exec } = require('child_process');
+
+require('dotenv').config();
+
+async function index() {
+  console.log('Deploying React Application');
+    // Generating the build files
+    exec('cd ../ && ls', (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        console.log('Error generating the build', err);
+        return;
+      }
+    
+      console.log(`stdout: ${stdout}`);
+      // console.log(`stderr: ${stderr}`);
+    });
+    
+
+    //  Connecting to the server to put the current build files
+    // ssh.connect({
+    //     host: process.env.SERVER_HOST,
+    //     username: process.env.SERVER_USER,
+    //     privateKey: `${path.join(__dirname, '..',`${process.env.SERVER_KEYFILE_NAME}.pem`)}`,
+    //   })
+    //   .then(() => {
+    //     console.log('Connected');
+    //     ssh.execCommand('pm2 list').then((result) => {
+    //       console.log('STDOUT: ' + result.stdout);
+    //     // console.log('STDERR: ' + result.stderr);
+    //     });
+    //   });
+};
+
+module.exports = index;
